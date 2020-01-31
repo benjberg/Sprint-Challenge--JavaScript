e946ee4bd880466769fb873ef83490b41136dfb3
Ben <bbjberg91@gmail.com>
2020-01-31 14:03:52 -0500

    gafdsg

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,7 +5,32 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
+const dinoOne = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  roar(){
+    return 'RAWERSRARARWERSARARARRRR!'
+  }
+}
 
+const dinoTwo = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic',
+}
+
+const dinoThree = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous',
+}
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -15,20 +40,20 @@
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinoOne.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinoTwo.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinoThree.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinoOne.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(dinoOne.roar());
 
 
 // ==== Arrays ====
@@ -51,7 +76,13 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+
+
+  const universities = graduates.filter(function(item){
+    return item === 'university';
+  });
+
+
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
